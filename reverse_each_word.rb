e86f263361c4e0804bbc9cdf7996325223577ffc
Garrett Thompson <gtcruz.gt@gmail.com>
2019-07-13 23:54:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,10 @@
 def reverse_each_word(string)
-  x = -1
   new_string = ""
   array_string = string.split
   array_string.each do |word|
-    new_string << " #{array_string[x]}"
-    x -= 1
+    new_string << " #{word}"
   end
-  return new_string
+  return new_string.reverse
 end
 
 def reverse_each_word(string)
