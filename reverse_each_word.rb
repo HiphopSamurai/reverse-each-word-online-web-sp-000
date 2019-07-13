a1d2e708b6778280c283552f980c6d1c622e0770
Garrett Thompson <gtcruz.gt@gmail.com>
2019-07-13 23:46:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,10 @@
 def reverse_each_word(string)
-  puts "#{string.reverse}"
+  x = -1
+  new_string = ""
+  array_string = string.split
+  array_string.each do |word|
+    new_string << " #{array_string[x]}"
+    x -= 1
+  end
+  puts new_string
 end
